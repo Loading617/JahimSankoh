73fa5db6cbd8759cea0df166715c310f15afe0a1
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-09-06 11:44:34 -0400

    Update funthings.php

--- funthings.php
+++ funthings.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Jahim Sankoh | Fun Things</title>
+<title>Jahim Sankoh's Official Site | Home</title>
 <meta charset="UTF-8">
 <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
@@ -14,37 +14,36 @@
 <body>
 
   <header>
+<?php echo "Introduction"; ?>
 
-
-    <div class="navbar">
+  <div class="navbar">
         <div class="navbar-logo">Jahim Sankoh</div>
         <nav>
-
-          <a href="home.php">Home</a>
-          <a href="funthings.php">Fun Things</a>
-          <a href="hobbies.php">Hobbies</a>
-          <a href="techstory.php">Tech Story</a>
-          <a href="contact.php">Contact</a>
+         <ul>
+          <li href="home.php">Home</li>
+          <li href="funthings.php">Fun Things</li>
+          <li href="hobbies.php">Hobbies</li>
+          <li href="techstory.php">Tech Story</li>
+          <li href="contact.php">Contact</li>
+          </ul>
             <span></span>
         
         </nav>
     
-
-    </div>
-        
-    <p>
-
+    <section>
     <div class="wrapper">
-    <img src="funthings.png" alt="funthings" width="405px" height="405px">
+    <img src="IMG_20220531_122425_177.jpg" alt="IMG" width="405px" height="405px">
     <div class="text-box">
-      <h1>Fun Things</h1>
-      <ul>
-        <li>Watching a Movie</li>
-        <li>Listening to Music</li>
-        <li>Play Dodgeball</li>
-        <li>Go on Roadtrips</li>
-      </ul>
-  </div>
-</div>
-</body>
+    <?php echo "Hi!&#128075; ya'll it's me Jahim Sankoh and Welcome to my Site!
+    I am a Software Developer from Philadelphia, Pennsylvania and Here your gonna learn more About me!
+    So Come On Join Cause and It's Gonna Be a Journey!"; ?>
+    </div>
+    </div>
+    </section>
+
+    <footer>
+       <?php echo "Jahim Sankoh &copy; 2023"; ?>
+    </footer>
+    </div>
+  </body>
 </html>
