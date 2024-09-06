19748fbda1b230c970cc94418fad3f9ca4659417
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-09-06 11:39:50 -0400

    Update and rename contact.php to contactinfo.php

---
+++ contactinfo.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Jahim Sankoh | Contact</title>
+<meta charset="UTF-8">
+<meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
+<link rel="icon" type="image/x-icon" href="J.ico">
+<link rel="stylesheet" type="text/css" href="styles.css">
+<link rel="stylesheet" type="text/css" href="navbar.css">
+<link rel="stylesheet" type="text/css" href="navbar-logo.css">
+<script type="text/javascript" src="fontsize.js"></script>
+</head>
+<body>
+
+  <header>
+<?php echo "Contact Information" ; ?>
+<div class="wrapper">
+    <img src="IMG_20220531_122425_177.jpg" alt="IMG" width="405px" height="405px">
+    <div class="text-box">
+    <?php echo "Email Me:" ; ?>
+    <?php echo "<a href="mailto:jahimsankoh319@gmail.com" target="_blank">jahimsankoh319@gmail.com</a>" ; ?>
+    <?php echo "Phone:" ; ?>
+    <?php echo "<a href="tel:803-846-0324" target="_blank">Call Me for Questions, Contributing to any of my Projects on GitHub, You can Reach Me Here: (803) 846-0324</a>" ; ?>
+    <h4>Socials:</h4>
+    <ul>
+      <li><a href="https://github.com/Loading617/" target="_blank"><img src="GitHub.png" alt="GitHub" width="32" height="32"></a></li>
+      <li><a href="https://www.facebook.com/jahim.sankoh.5" target="_blank"><img src="facebook.png" alt="Facebook" width="32" height="32"></a></li>
+      <li><a href="https://www.instagram.com/jusbeing.jahim/" target="_blank"><img src="instagram.png" alt="Instagram" width="32" height="32"></a></li>
+      <li><a href="https://www.twitter.com/jahim4x" target="_blank"><img src="x.png" alt="X formely Twitter" width="32" height="32"></a></li>
+
+    <div class="navbar">
+        <div class="navbar-logo">Jahim Sankoh</div>
+        <nav>
+
+          <a href="home.php">Home</a>
+          <a href="funthings.php">Fun Things</a>
+          <a href="hobbies.php">Hobbies</a>
+          <a href="techstory.php">Tech Story</a>
+          <a href="contact.php">Contact</a>
+            <span></span>
+        
+        </nav>
+    
+
+    </div>
+        
+</div>
+</body>
+</html>
