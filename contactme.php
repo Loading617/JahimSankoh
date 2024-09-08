01622517384c8edf4a09c4e3f6e20da59fc67b81
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-09-08 04:53:19 -0400

    Update contactme.php

--- contactme.php
+++ contactme.php
@@ -14,31 +14,36 @@
 <body>
 
   <header>
-<?php echo "Contact Information" ; ?>
+<?php echo "<h1>Contact Information</h1>"; ?>
 <div class="wrapper">
     <img src="IMG_20220531_122425_177.jpg" alt="IMG" width="405px" height="405px">
     <div class="text-box">
-    <?php echo "Email Me:" ; ?>
-    <?php echo "<a href="mailto:jahimsankoh319@gmail.com" target="_blank">jahimsankoh319@gmail.com</a>" ; ?>
-    <?php echo "Phone:" ; ?>
-    <?php echo "<a href="tel:803-846-0324" target="_blank">Call Me for Questions, Contributing to any of my Projects on GitHub, You can Reach Me Here: (803) 846-0324</a>" ; ?>
-    <h4>Socials:</h4>
+    <form action="contact.php" method="POST">
+    Email Me: <input type="text" name="email"><br>
+    <?php echo "<p><li href="mailto:jahimsankoh319@gmail.com" target="_blank">jahimsankoh319@gmail.com</p></li>"; ?>
+    Phone: <input type="text" name="phone"><br>
+    </form>
+    <?php echo "<p><li href="tel:803-846-0324" target="_blank">Call Me for Questions, Contributing to any of my Projects on GitHub, You can Reach Me Here: (803) 846-0324</p></li>"; ?>
+    <?php echo "<h4>Socials:</h4>"; ?>
+    <?php echo "<h5>E-mail:</h5>"; ?>
     <ul>
+    <p><li href="mailto:jahimsankoh319@gmail.com" target="_blank">jahimsankoh319@gmail.com</p></li>
+    </ul>
+      <ul>
       <li><a href="https://github.com/Loading617/" target="_blank"><img src="GitHub.png" alt="GitHub" width="32" height="32"></a></li>
       <li><a href="https://www.facebook.com/jahim.sankoh.5" target="_blank"><img src="facebook.png" alt="Facebook" width="32" height="32"></a></li>
       <li><a href="https://www.instagram.com/jusbeing.jahim/" target="_blank"><img src="instagram.png" alt="Instagram" width="32" height="32"></a></li>
       <li><a href="https://www.twitter.com/jahim4x" target="_blank"><img src="x.png" alt="X formely Twitter" width="32" height="32"></a></li>
-
+      </ul>
     <div class="navbar">
         <div class="navbar-logo">Jahim Sankoh</div>
         <nav>
-          <ul>
-          <li href="home.php">Home</li>
-          <li href="funthings.php">Fun Things</li>
-          <li href="hobbies.php">Hobbies</li>
-          <li href="techstory.php">Tech Story</li>
-          <li href="contact.php">Contact</li>
-            </ul>
+
+          <a href="home.php">Home</a>
+          <a href="funthings.php">Fun Things</a>
+          <a href="hobbies.php">Hobbies</a>
+          <a href="techstory.php">Tech Story</a>
+          <a href="contactme.php">Contact Me</a>
             <span></span>
         
         </nav>
