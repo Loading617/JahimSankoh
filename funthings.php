f3a5c9bee88e90dfd9b164609efd7650b5211c0c
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-09-17 13:13:19 -0400

    Update funthings.php

--- funthings.php
+++ funthings.php
@@ -15,7 +15,7 @@
 
   <header>
 
-  <?php echo "<h1>Fun Things</h1>"; ?>
+  <h1>Fun Things</h1>
   <div class="wrapper">
     <img src="funthings.png" alt="funthings" width="405px" height="405px">
     <div class="text-box"></div>
