8da34883bffdeaecea0de7526e0fc34bd0067780
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-09-17 13:18:27 -0400

    Update contactme.php

--- contactme.php
+++ contactme.php
@@ -14,15 +14,15 @@
 <body>
 
   <header>
-<?php echo "<h1>Contact Information</h1>"; ?>
+<h1>Contact Information</h1>
 <div class="wrapper">
     <img src="IMG_20220531_122425_177.jpg" alt="IMG" width="405px" height="405px">
     <div class="text-box">
-    <?php echo "<h2>Email Me:</h2>"; ?>
-    <?php echo "<p><li href="mailto:jahimsankoh319@gmail.com" target="_blank">jahimsankoh319@gmail.com</p></li>"; ?>
-    <?php echo "<h3>Phone:</h3>"; ?>
-    <?php echo "<p><li href="tel:803-846-0324" target="_blank">Call Me for Questions, Contributing to any of my Projects on GitHub, You can Reach Me Here: (803) 846-0324</p></li>"; ?>
-    <?php echo "<h4>Socials:</h4>"; ?>
+    <h2>Email Me:</h2>
+    <p><li href="mailto:jahimsankoh319@gmail.com" target="_blank">jahimsankoh319@gmail.com</p></li>
+    <h3>Phone:</h3>
+    <p><li href="tel:803-846-0324" target="_blank">Call Me for Questions, Contributing to any of my Projects on GitHub, You can Reach Me Here: (803) 846-0324</p></li>"; ?>
+    <h4>Socials:</h4>
       <ul>
       <li><a href="https://github.com/Loading617/" target="_blank"><img src="GitHub.png" alt="GitHub" width="32" height="32"></a></li>
       <li><a href="https://www.facebook.com/jahim.sankoh.5" target="_blank"><img src="facebook.png" alt="Facebook" width="32" height="32"></a></li>
