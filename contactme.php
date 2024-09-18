7eddec4d2140e45a26581a557adbb1fad0285706
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-09-17 21:10:07 -0400

    Update contactme.php

--- contactme.php
+++ contactme.php
@@ -1,7 +1,7 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html lang="en">
 <head>
-<title>Jahim Sankoh | Contact Info</title>
+<title>Jahim Sankoh - Contact Me</title>
 <meta charset="UTF-8">
 <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
@@ -14,15 +14,15 @@
 <body>
 
   <header>
-<h1>Contact Information</h1>
+<?php echo"<h1>Contact Information</h1>"; ?>
 <div class="wrapper">
     <img src="IMG_20220531_122425_177.jpg" alt="IMG" width="405px" height="405px">
     <div class="text-box">
-    <h2>Email Me:</h2>
-    <p><a href="mailto:jahimsankoh319@gmail.com" target="_blank">jahimsankoh319@gmail.com</p></li>
-    <h3>Phone:</h3>
-    <p><a href="tel:803-846-0324" target="_blank">Call Me for Questions, Contributing to any of my Projects on GitHub, You can Reach Me Here: (803) 846-0324</p></li>"; ?>
-    <h4>Socials:</h4>
+    <?php echo"<h2>Email Me:</h2>"; ?>
+    <?php echo"<p><li href="mailto:jahimsankoh319@gmail.com" target="_blank">jahimsankoh319@gmail.com</p></li>"; ?>
+    <?php echo"<h3>Phone:</h3>"; ?>
+    <?php echo"<p><li href="tel:803-846-0324" target="_blank">Call Me for Questions, Contributing to any of my Projects on GitHub, You can Reach Me Here: (803) 846-0324</p></li>"; ?>
+    <?php echo"<h4>Socials:</h4>"; ?>
       <ul>
       <li><a href="https://github.com/Loading617/" target="_blank"><img src="GitHub.png" alt="GitHub" width="32" height="32"></a></li>
       <li><a href="https://www.facebook.com/jahim.sankoh.5" target="_blank"><img src="facebook.png" alt="Facebook" width="32" height="32"></a></li>
@@ -33,12 +33,11 @@
         <div class="navbar-logo">Jahim Sankoh</div>
         <nav>
 
-          <a href="home.php">Home</a>
-          <a href="funthings.php">Fun Things</a>
-          <a href="hobbies.php">Hobbies</a>
-          <a href="techstory.php">Tech Story</a>
-          <a href="contactme.php">Contact Me</a>
-            <span></span>
+          <li href="home.php">Home</li>
+          <li href="funthings.php">Fun Things</li>
+          <li href="hobbies.php">Hobbies</li>
+          <li href="techstory.php">Tech Story</li>
+          <li href="contactme.php">Contact Me</li>
         
         </nav>
     
