be145ffc572160c89c04582bc11dafff2f5764da
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-09-24 21:30:36 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,40 +6,56 @@
 <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
 <link rel="icon" type="image/x-icon" href="J.ico">
-<link rel="stylesheet" type="text/css" href="styles.css">
-<link rel="stylesheet" type="text/css" href="navbar.css">
-<link rel="stylesheet" type="text/css" href="navbar-logo.css">
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<link rel="stylesheet" type="text/css" href="css/navbar.css">
+<link rel="stylesheet" type="text/css" href="css/navbar-logo.css">
+<link rel="stylesheet" type="text/css" href="css/image.css">
+<link rel="stylesheet" type="text/css" href="css/text.css">
 <script type="text/javascript" src="fontsize.js"></script>
 </head>
 <body>
 
-  <header>
-<?php echo"<h1>Introduction</h1>"; ?>
-
   <div class="navbar">
         <div class="navbar-logo">Jahim Sankoh</div>
         <nav>
-          <li href="home.php">Home</li>
-          <li href="funthings.php">Fun Things</li>
-          <li href="hobbies.php">Hobbies</li>
-          <li href="techstory.php">Tech Story</li>
-          <li href="contactme.php">Contact Me</li>
+          <a href="index.php">Home</a>
+          <a href="funthings.php">Fun Things</a>
+          <a href="hobbies.php">Hobbies</a>
+          <a href="techstory.php">Tech Story</a>
+          <a href="contactme.php">Contact Me</a>
         </nav>
     
-    <section>
     <div class="wrapper">
-    <img src="IMG_20220531_122425_177.jpg" alt="IMG" width="405px" height="405px">
+    <section class="wrapper">
+    <picture>
+    <source media="(min-width: 40em)" srcset="IMG_20220531_122425_177.jpg" width="1058" height="1086">
+    <img src="IMG_20220531_122425_177.jpg" alt="IMG" width="1200" height="812">
+    </picture>
     <div class="text-box">
+    <?php echo"<h1>Introduction</h1>"; ?>
     <?php echo"<p>Hi!&#128075; ya'll it's me Jahim Sankoh and Welcome to my Site!
     I am a Software Developer from Philadelphia, Pennsylvania and Here your gonna learn more About me!
     So Come On Join Cause and It's Gonna Be a Journey!</p>"; ?>
     </div>
-    </div>
     </section>
-
-    <footer>
-       <?php echo"<p>Jahim Sankoh &copy; 2023-2024</p>"; ?>
-    </footer>
     </div>
+
+    <style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: rgb(38, 208, 206);
+   color: navy;
+   text-align: center;
+}
+</style>
+</head>
+<body>
+
+<div class="footer">
+  <?php echo"<p>Jahim Sankoh &copy; 2023-2024</p>"; ?>
+</div>
   </body>
 </html>
