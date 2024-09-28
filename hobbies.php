2538d62a2c2ab027827b3029fdc73d5a60be4401
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-09-28 18:34:30 -0400

    Update hobbies.php

--- hobbies.php
+++ hobbies.php
@@ -7,8 +7,6 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
 <link rel="icon" type="image/x-icon" href="J.ico">
 <link rel="stylesheet" type="text/css" href="styles.css">
-<link rel="stylesheet" type="text/css" href="navbar.css">
-<link rel="stylesheet" type="text/css" href="navbar-logo.css">
 <script type="text/javascript" src="fontsize.js"></script>
 </head>
 <body>
