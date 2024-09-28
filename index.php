d670138dd6795b1a3fb49660ec281b46ae16400a
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-09-28 15:41:41 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,54 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Jahim Sankoh's Official Site - Home</title>
-<meta charset="UTF-8">
-<meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
-<link rel="icon" type="image/x-icon" href="J.ico">
-<link rel="stylesheet" type="text/css" href="css/styles.css">
-<link rel="stylesheet" type="text/css" href="css/navbar.css">
-<link rel="stylesheet" type="text/css" href="css/navbar-logo.css">
-<script type="text/javascript" src="fontsize.js"></script>
+    <title>Jahim Sankoh's Official Site - Home</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" type="image/x-icon" href="J.ico">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <script type="text/javascript" src="fontsize.js"></script>
 </head>
 <body>
+    <div class="container">
+        <header>
+            <div class="navbar-logo">Jahim Sankoh</div>
+            <nav>
+                <a href="index.php">Home</a>
+                <a href="funthings.php">Fun Things</a>
+                <a href="hobbies.php">Hobbies</a>
+                <a href="techstory.php">Tech Story</a>
+                <a href="contactme.php">Contact Me</a>
+            </nav>
+        </header>
+        
+        <main>
+            <div class="content-wrapper">
+                <img src="IMG_20220531_122425_177.jpg" alt="IMG" width="405" height="405">
+                <div class="text-box">
+                    <h1>Introduction</h1>
+                    <p>Hi!&#128075; ya'll it's me Jahim Sankoh and Welcome to my Site!
+                    I am a Software Developer from Philadelphia, Pennsylvania and Here your gonna learn more About me!
+                    So Come On Join Cause and It's Gonna Be a Journey!</p>
+                </div>
+            </div>
+        </main>
 
-  <div class="navbar">
-        <div class="navbar-logo">Jahim Sankoh</div>
-        <nav>
-          <a href="index.php">Home</a>
-          <a href="funthings.php">Fun Things</a>
-          <a href="hobbies.php">Hobbies</a>
-          <a href="techstory.php">Tech Story</a>
-          <a href="contactme.php">Contact Me</a>
-        </nav>
-    
-    <div class="wrapper">
-    <img src="IMG_20220531_122425_177.jpg" alt="IMG" width="405px" height="405px">
-    <div class="text-box">
-    <?php echo"<h1>Introduction</h1>"; ?>
-    <?php echo"<p>Hi!&#128075; ya'll it's me Jahim Sankoh and Welcome to my Site!
-    I am a Software Developer from Philadelphia, Pennsylvania and Here your gonna learn more About me!
-    So Come On Join Cause and It's Gonna Be a Journey!</p>"; ?>
+        <footer>
+            <p>Jahim Sankoh &copy; 2023-2024</p>
+        </footer>
     </div>
-    </div>
-
-    <style>
-.footer {
-   position: fixed;
-   left: 0;
-   bottom: 0;
-   width: 100%;
-   background-color: rgb(38, 208, 206);
-   color: navy;
-   text-align: center;
-}
-</style>
-</head>
-<body>
-
-<div class="footer">
-  <?php echo"<p>Jahim Sankoh &copy; 2023-2024</p>"; ?>
-</div>
-  </body>
+</body>
 </html>
