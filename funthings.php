6b566f0e8bf32754efa1902ee36fb0dcfc51ab49
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-10-09 21:31:13 -0400

    Update funthings.php

--- funthings.php
+++ funthings.php
@@ -6,29 +6,30 @@
 <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
 <link rel="icon" type="image/x-icon" href="J.ico">
-<link rel="stylesheet" type="text/css" href="styles.css">
+<link rel="stylesheet" type="text/css" href="css/styles.css">
 <script type="text/javascript" src="fontsize.js"></script>
 </head>
 <body>
-<div class="container">
+<div class="content-wrapper">
   <header>
     <div class="navbar-logo">Jahim Sankoh</div>
     <nav>
-      <a href="index.php">Home</a>
-      <a href="aboutme.php">About Me</a>
-      <a href="hobbies.php">Hobbies</a>
-      <a href="techstory.php">Tech Story</a>
-      <a href="contactme.php">Contact Me</a>
+        <a href="index.php">Home</a>
+        <a href="funthings.php">Fun Things</a>
+        <a href="hobbies.php">Hobbies</a>
+        <a href="techstory.php">Tech Story</a>
+        <a href="contactme.php">Contact Me</a>
     </nav>
     </div>
   </header>
 
   <main>
-    
   <div class="content-wrapper">
+    <div class="image-container">
     <img src="funthings.png" alt="Fun Things" width="405px" height="405px">
+    </div>
     <div class="text-box">
-      <?php echo"<h1>Fun Things</h1>"; 
+    <?php echo"<h1>Fun Things</h1>"; ?>
       <?php
       echo "<ul>";
       echo "<li>Watching a Movie</li>";
