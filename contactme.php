cd215b1da490d841cc6c498f237ddc3f37804865
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-10-10 16:00:50 -0400

    Update contactme.php

--- contactme.php
+++ contactme.php
@@ -4,7 +4,58 @@
 <title>Jahim Sankoh - Contact Me</title>
 <meta charset="UTF-8">
 <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge Web<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<title>Jahim Sankoh - Contact Me</title>
+<meta charset="UTF-8">
+<meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
+<link rel="icon" type="image/x-icon" href="J.ico">
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<script type="text/javascript" src="fontsize.js"></script>
+</head>
+<body>
+    <div class="container">
+        <header>
+        <div class="navbar-logo">Jahim Sankoh</div>
+        <nav>
+          <a href="home.php">Home</a>
+          <a href="funthings.php">Fun Things</a>
+          <a href="hobbies.php">Hobbies</a>
+          <a href="techstory.php">Tech Story</a>
+          <a href="contactme.php">Contact Me</a>
+        </nav>
+        </header>
+
+        <main>
+            <div class="content-wrapper">
+                <div class="image-container">
+                <img src="IMG_20220531_122425_177.jpg" alt="Picture of Me" width="405" height="405">
+                </div>
+                <div class="text-box">
+                <h1>Contact Information</h1>
+                <div class="contact-info">
+                <?php echo"<h2>Email Me:</h2>"; ?>
+    <p><a href="mailto:jahimsankoh319@gmail.com" target="_blank">jahimsankoh319@gmail.com</a>
+    <?php echo"<h3>Phone:</h3>"; ?>
+    <p>Call Me for Questions, Contributing to any of my Projects on GitHub, You can Reach Me Here:</p>
+    <a href="tel:803-846-0324" target="_blank">(803) 846-0324</a>
+    <div class="content-wrapper">
+    <?php echo"<h4>Socials:</h4>"; ?>
+    <p><a href="https://github.com/Loading617/" target="_blank"><img src="GitHub.png" alt="GitHub" width="32" height="32"></a></p>
+    <p><a href="https://www.facebook.com/jahim.sankoh.5" target="_blank"><img src="facebook.png" alt="Facebook" width="32" height="32"></a></p>
+    <p><a href="https://www.instagram.com/jusbeing.jahim/" target="_blank"><img src="instagram.png" alt="Instagram" width="32" height="32"></a></p>
+    <p><a href="https://www.twitter.com/jahim4x" target="_blank"><img src="x.png" alt="X formely Twitter" width="32" height="32"></a></p>
+                </div>
+                </div>
+                </div>
+        </main>
+        <footer>
+            <p>&copy; 2023 Jahim Sankoh. All rights reserved.</p>
+        </footer>
+</body>
+</html>kit, Chrome= Edge">
 <link rel="icon" type="image/x-icon" href="J.ico">
 <link rel="stylesheet" type="text/css" href="styles.css">
 <script type="text/javascript" src="fontsize.js"></script>
