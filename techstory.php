0f657d28bab5a920b5f78f804dda2ad413be6ac7
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-10-09 21:13:10 -0400

    Update techstory.php

--- techstory.php
+++ techstory.php
@@ -1,52 +1,53 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-<title>Jahim Sankoh - Tech Story</title>
-<meta charset="UTF-8">
-<meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
-<link rel="icon" type="image/x-icon" href="J.ico">
-<link rel="stylesheet" type="text/css" href="styles.css">
-<script type="text/javascript" src="fontsize.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
+    <title>Jahim Sankoh - Tech Story</title>
+    <link rel="icon" type="image/x-icon" href="J.ico">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <script type="text/javascript" src="fontsize.js"></script>
 </head>
 <body>
-
-  <header>
-  <?php echo"<h1>Tech Story</h1>"; ?>
-  <div class="content-wrapper">
-    <img src="IMG_20220531_122425_177.jpg" alt="IMG" width="405px" height="405px">
-    <div class="text-box"></div>
-<?php echo"<p>First in Elementary School, I Learned on Chromebook how to Find a Image and Set as Background.
-      Next in Elementary School, I Learned How to Change Themes in Chrome.
-      Third in Elementary School, I Learned How to Add Extensions in Chrome.
-      Initially In Elementary School, I Learned How to get apps in Chrome Web Store.
-      Plus in Elementary School, I Learned How to get Games from the Chrome Web Store.
-      Too, in Elementary School, I Found Flash Game Websites in Chrome like newstudyhall & The School Study Hall.      
-      Last in Elementary School, I Learned How to Download MP3 Files from a Website Called Starfall.
-      Now Moving on to Middle School...
-      Beginning in Middle School, I Learned How to Download YouTube Videos to MP4 from a Website from Multiple Websites like PlayitHub, PointMP4, convert2mp3.net.
-      Further in Middle School, I Found More Sites to Download MP3 Files from Websites called JungleVibe, pointmp3.com, YouZik.
-      Also in Middle School, I Learned How to Download a Emulator from Chrome Web Store called GPEMU.
-      Lastly Then in Middle School, I Found the first ROM Site to Download ROMS for GPEMU.
-      Moving on to High School...
-      In High School, I Learned How to Download APKS and Open An APK Using ARC Welder.</p>"; ?>
-      </div>
-      </div>
-
-    <div class="navbar">
+    <header>
         <div class="navbar-logo">Jahim Sankoh</div>
         <nav>
-        <ul>
-          <li href="home.php">Home</li>
-          <li href="funthings.php">Fun Things</li>
-          <li href="hobbies.php">Hobbies</li>
-          <li href="techstory.php">Tech Story</li>
-          <li href="contactme.php">Contact Me</li>
-          </ul>
-        
+            <a href="index.php">Home</a>
+            <a href="funthings.php">Fun Things</a>
+            <a href="hobbies.php">Hobbies</a>
+            <a href="techstory.php">Tech Story</a>
+            <a href="contactme.php">Contact Me</a>
         </nav>
-    
-    </div>
+    </header>
 
+    <main>
+        <div class="content-wrapper">
+            <div class="image-container">
+                <img src="IMG_20220531_122425_177.jpg" alt="Picture of Me" width="405" height="405">
+            </div>
+            <div class="text-box">
+                <?php echo"<h1>Tech Story</h1>"; ?>
+                <?php echo "<p>First in Elementary School, I Learned on Chromebook how to Find a Image and Set as Background.
+                Next in Elementary School, I Learned How to Change Themes in Chrome.
+                Third in Elementary School, I Learned How to Add Extensions in Chrome.
+                Initially In Elementary School, I Learned How to get apps in Chrome Web Store.
+                Plus in Elementary School, I Learned How to get Games from the Chrome Web Store.
+                Too, in Elementary School, I Found Flash Game Websites in Chrome like newstudyhall & The School Study Hall.
+                Last in Elementary School, I Learned How to Download MP3 Files from a Website Called Starfall.
+                Now Moving on to Middle School...
+                Beginning in Middle School, I Learned How to Download YouTube Videos to MP4 from a Website from Multiple Websites like PlayitHub, PointMP4, convert2mp3.net.
+                Further in Middle School, I Found More Sites to Download MP3 Files from Websites called JungleVibe, pointmp3.com, YouZik.
+                Also in Middle School, I Learned How to Download a Emulator from Chrome Web Store called GPEMU.
+                Lastly Then in Middle School, I Found the first ROM Site to Download ROMS for GPEMU.
+                Moving on to High School...
+                In High School, I Learned How to Download APKS and Open An APK Using ARC Welder.</p>"; ?>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <p>&copy; 2024 Jahim Sankoh. All rights reserved.</p>
+    </footer>
 </body>
 </html>
