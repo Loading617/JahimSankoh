8450054e138b0252a299bbd0dd93449740721cab
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-10-10 16:00:13 -0400

    Update techstory.php

--- techstory.php
+++ techstory.php
@@ -1,52 +1,53 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-<title>Jahim Sankoh - Contact Me</title>
-<meta charset="UTF-8">
-<meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
-<link rel="icon" type="image/x-icon" href="J.ico">
-<link rel="stylesheet" type="text/css" href="css/styles.css">
-<script type="text/javascript" src="fontsize.js"></script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
+    <title>Jahim Sankoh - Tech Story</title>
+    <link rel="icon" type="image/x-icon" href="J.ico">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <script type="text/javascript" src="fontsize.js"></script>
 </head>
 <body>
-    <div class="container">
-        <header>
+    <header>
         <div class="navbar-logo">Jahim Sankoh</div>
         <nav>
-          <a href="home.php">Home</a>
-          <a href="funthings.php">Fun Things</a>
-          <a href="hobbies.php">Hobbies</a>
-          <a href="techstory.php">Tech Story</a>
-          <a href="contactme.php">Contact Me</a>
+            <a href="index.php">Home</a>
+            <a href="funthings.php">Fun Things</a>
+            <a href="hobbies.php">Hobbies</a>
+            <a href="techstory.php">Tech Story</a>
+            <a href="contactme.php">Contact Me</a>
         </nav>
-        </header>
+    </header>
 
-        <main>
-            <div class="content-wrapper">
-                <div class="image-container">
+    <main>
+        <div class="content-wrapper">
+            <div class="image-container">
                 <img src="IMG_20220531_122425_177.jpg" alt="Picture of Me" width="405" height="405">
-                </div>
-                <div class="text-box">
-                <h1>Contact Information</h1>
-                <div class="contact-info">
-                <?php echo"<h2>Email Me:</h2>"; ?>
-    <p><a href="mailto:jahimsankoh319@gmail.com" target="_blank">jahimsankoh319@gmail.com</a>
-    <?php echo"<h3>Phone:</h3>"; ?>
-    <p>Call Me for Questions, Contributing to any of my Projects on GitHub, You can Reach Me Here:</p>
-    <a href="tel:803-846-0324" target="_blank">(803) 846-0324</a>
-    <div class="content-wrapper">
-    <?php echo"<h4>Socials:</h4>"; ?>
-    <p><a href="https://github.com/Loading617/" target="_blank"><img src="GitHub.png" alt="GitHub" width="32" height="32"></a></p>
-    <p><a href="https://www.facebook.com/jahim.sankoh.5" target="_blank"><img src="facebook.png" alt="Facebook" width="32" height="32"></a></p>
-    <p><a href="https://www.instagram.com/jusbeing.jahim/" target="_blank"><img src="instagram.png" alt="Instagram" width="32" height="32"></a></p>
-    <p><a href="https://www.twitter.com/jahim4x" target="_blank"><img src="x.png" alt="X formely Twitter" width="32" height="32"></a></p>
-                </div>
-                </div>
-                </div>
-        </main>
-        <footer>
-            <p>&copy; 2023 Jahim Sankoh. All rights reserved.</p>
-        </footer>
+            </div>
+            <div class="text-box">
+                <?php echo"<h1>Tech Story</h1>"; ?>
+                <?php echo "<p>First in Elementary School, I Learned on Chromebook how to Find a Image and Set as Background.
+                Next in Elementary School, I Learned How to Change Themes in Chrome.
+                Third in Elementary School, I Learned How to Add Extensions in Chrome.
+                Initially In Elementary School, I Learned How to get apps in Chrome Web Store.
+                Plus in Elementary School, I Learned How to get Games from the Chrome Web Store.
+                Too, in Elementary School, I Found Flash Game Websites in Chrome like newstudyhall & The School Study Hall.
+                Last in Elementary School, I Learned How to Download MP3 Files from a Website Called Starfall.
+                Now Moving on to Middle School...
+                Beginning in Middle School, I Learned How to Download YouTube Videos to MP4 from a Website from Multiple Websites like PlayitHub, PointMP4, convert2mp3.net.
+                Further in Middle School, I Found More Sites to Download MP3 Files from Websites called JungleVibe, pointmp3.com, YouZik.
+                Also in Middle School, I Learned How to Download a Emulator from Chrome Web Store called GPEMU.
+                Lastly Then in Middle School, I Found the first ROM Site to Download ROMS for GPEMU.
+                Moving on to High School...
+                In High School, I Learned How to Download APKS and Open An APK Using ARC Welder.</p>"; ?>
+            </div>
+        </div>
+    </main>
+
+    <footer>
+        <p>&copy; 2024 Jahim Sankoh. All rights reserved.</p>
+    </footer>
 </body>
 </html>
