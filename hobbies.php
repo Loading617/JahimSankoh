9e4dea275c4927dad0dd27102e13e3af1f43cdf6
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-10-09 21:32:04 -0400

    Update hobbies.php

--- hobbies.php
+++ hobbies.php
@@ -6,39 +6,41 @@
 <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
 <link rel="icon" type="image/x-icon" href="J.ico">
-<link rel="stylesheet" type="text/css" href="styles.css">
+<link rel="stylesheet" type="text/css" href="css/styles.css">
 <script type="text/javascript" src="fontsize.js"></script>
 </head>
 <body>
-
+<div class="content-wrapper">
   <header>
-
-  <?php echo"<h1>Hobbies</h1>"; ?>
-  <div class="wrapper">
-    <img src="hobbies.png" alt="IMG" width="405px" height="405px">
-    <div class="text-box">
-      <ul>
-        <li>Dancing</li>
-        <li>Acting</li>
-        <li>Tech</li>
-        <li>Shopping</li>
-      </ul>
-      </div>
-      </div>
-
-    <div class="navbar">
-        <div class="navbar-logo">Jahim Sankoh</div>
-        <nav>
-        <ul>
-          <li href="home.html">Home</li>
-          <li href="funthings.php">Fun Things</li>
-          <li href="hobbies.php">Hobbies</li>
-          <li href="techstory.php">Tech Story</li>
-          <li href="contactme.php">Contact Me</li>
-          </ul>
-        </nav>
-    
+    <div class="navbar-logo">Jahim Sankoh</div>
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="funthings.php">Fun Things</a>
+        <a href="hobbies.php">Hobbies</a>
+        <a href="techstory.php">Tech Story</a>
+        <a href="contactme.php">Contact Me</a>
+    </nav>
     </div>
+  </header>
+
+  <main>
+  <div class="content-wrapper">
+    <div class="image-container">
+    <img src="hobbies.png" alt="Fun Things" width="405px" height="405px">
+    </div>
+    <div class="text-box">
+    <?php echo"<h1>Hobbies</h1>"; ?>
+      <?php
+      echo "<ul>";
+      echo "<li>Dancing</li>";
+      echo "<li>Acting</li>";
+      echo "<li>Tech</li>";
+      echo "<li>Shopping</li>";
+      echo "</ul>";
+      ?>
+      </div>
+      </div>
+      </main>
 
 </body>
 </html>
