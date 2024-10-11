b0530d0c0279abac9f51a658cfac1f86a2f2ba48
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-10-11 11:31:40 -0400

    Update contactme.php

--- contactme.php
+++ contactme.php
@@ -1,51 +1,56 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
-<title>Jahim Sankoh - Contact Me</title>
-<meta charset="UTF-8">
-<meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
-<link rel="icon" type="image/x-icon" href="J.ico">
-<link rel="stylesheet" type="text/css" href="css/styles.css">
-<script type="text/javascript" src="fontsize.js"></script>
+    <title>Jahim Sankoh - Contact Me</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome=Edge">
+    <link rel="icon" type="image/x-icon" href="J.ico">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <script type="text/javascript" src="fontsize.js"></script>
+    <script type="text/javascript" src="paragraph.js"></script>
 </head>
 <body>
     <div class="container">
         <header>
-        <div class="navbar-logo">Jahim Sankoh</div>
-        <nav>
-          <a href="home.php">Home</a>
-          <a href="funthings.php">Fun Things</a>
-          <a href="hobbies.php">Hobbies</a>
-          <a href="techstory.php">Tech Story</a>
-          <a href="contactme.php">Contact Me</a>
-        </nav>
+            <div class="navbar-logo">Jahim Sankoh</div>
+            <nav>
+                <a href="home.php">Home</a>
+                <a href="funthings.php">Fun Things</a>
+                <a href="hobbies.php">Hobbies</a>
+                <a href="techstory.php">Tech Story</a>
+                <a href="contactme.php">Contact Me</a>
+            </nav>
         </header>
 
         <main>
             <div class="content-wrapper">
                 <div class="image-container">
-                <img src="IMG_20220531_122425_177.jpg" alt="Picture of Me" width="405" height="405">
+                    <img src="IMG_20220531_122425_177.jpg" alt="Picture of Me" width="405" height="405">
                 </div>
                 <div class="text-box">
-                <h1>Contact Information</h1>
-                <div class="contact-info">
-                <?php echo"<h2>Email Me:</h2>"; ?>
-    <p><a href="mailto:jahimsankoh319@gmail.com" target="_blank">jahimsankoh319@gmail.com</a></p>
-    <?php echo"<h3>Phone:</h3>"; ?>
-    <p>Call Me for Questions, Contributing to any of my Projects on GitHub, You can Reach Me Here:</p>
-    <a href="tel:803-846-0324" target="_blank">(803) 846-0324</a>
-    <div class="content-wrapper">
-    <?php echo"<h4>Socials:</h4>"; ?>
-    <a href="https://github.com/Loading617/" target="_blank"><img src="GitHub.png" alt="GitHub" width="32" height="32"></a>
-    <a href="https://www.facebook.com/jahim.sankoh.5" target="_blank"><img src="facebook.png" alt="Facebook" width="32" height="32"></a>
-    <a href="https://www.instagram.com/jusbeing.jahim/" target="_blank"><img src="instagram.png" alt="Instagram" width="32" height="32"></a>
-    <a href="https://www.twitter.com/jahim4x" target="_blank"><img src="x.png" alt="X formely Twitter" width="32" height="32"></a>
+                    <h1>Contact Information</h1>
+                    <div class="contact-info">
+                        <h2>Email Me:</h2>
+                        <p><a href="mailto:jahimsankoh319@gmail.com" target="_blank">jahimsankoh319@gmail.com</a></p>
+                        <h3>Phone:</h3>
+                        <p>Call Me for Questions, Contributing to any of my Projects on GitHub, You can Reach Me Here:</p>
+                        <a href="tel:803-846-0324" target="_blank">(803) 846-0324</a>
+                        <div class="socials">
+                            <h4>Socials:</h4>
+                            <a href="https://github.com/Loading617/" target="_blank"><i class="fab fa-github"></i></a>
+                            <a href="https://www.facebook.com/jahim.sankoh.5" target="_blank"><i class="fab fa-facebook"></i></a>
+                            <a href="https://www.instagram.com/jusbeing.jahim/" target="_blank"><i class="fab fa-instagram"></i></a>
+                            <a href="https://www.twitter.com/jahim4x" target="_blank"><i class="fab fa-twitter"></i></a>
+                        </div>
+                    </div>
                 </div>
-                </div>
+            </div>
         </main>
         <footer>
             <p>&copy; 2023 Jahim Sankoh. All rights reserved.</p>
         </footer>
+    </div>
 </body>
 </html>
