d67e85afd96f405f013ba69982ec24e9261e2884
Jahim Sankoh <89672772+Loading617@users.noreply.github.com>
2024-10-11 02:06:30 -0400

    Update contactme.php

--- contactme.php
+++ contactme.php
@@ -4,12 +4,6 @@
 <title>Jahim Sankoh - Contact Me</title>
 <meta charset="UTF-8">
 <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="X-UA-Compatible" content="IE=edge Web<!DOCTYPE HTML>
-<html lang="en">
-<head>
-<title>Jahim Sankoh - Contact Me</title>
-<meta charset="UTF-8">
-<meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="X-UA-Compatible" content="IE=edge Webkit, Chrome= Edge">
 <link rel="icon" type="image/x-icon" href="J.ico">
 <link rel="stylesheet" type="text/css" href="css/styles.css">
@@ -37,16 +31,16 @@
                 <h1>Contact Information</h1>
                 <div class="contact-info">
                 <?php echo"<h2>Email Me:</h2>"; ?>
-    <p><a href="mailto:jahimsankoh319@gmail.com" target="_blank">jahimsankoh319@gmail.com</a>
+    <p><a href="mailto:jahimsankoh319@gmail.com" target="_blank">jahimsankoh319@gmail.com</a></p>
     <?php echo"<h3>Phone:</h3>"; ?>
     <p>Call Me for Questions, Contributing to any of my Projects on GitHub, You can Reach Me Here:</p>
     <a href="tel:803-846-0324" target="_blank">(803) 846-0324</a>
     <div class="content-wrapper">
     <?php echo"<h4>Socials:</h4>"; ?>
-    <p><a href="https://github.com/Loading617/" target="_blank"><img src="GitHub.png" alt="GitHub" width="32" height="32"></a></p>
-    <p><a href="https://www.facebook.com/jahim.sankoh.5" target="_blank"><img src="facebook.png" alt="Facebook" width="32" height="32"></a></p>
-    <p><a href="https://www.instagram.com/jusbeing.jahim/" target="_blank"><img src="instagram.png" alt="Instagram" width="32" height="32"></a></p>
-    <p><a href="https://www.twitter.com/jahim4x" target="_blank"><img src="x.png" alt="X formely Twitter" width="32" height="32"></a></p>
+    <a href="https://github.com/Loading617/" target="_blank"><img src="GitHub.png" alt="GitHub" width="32" height="32"></a>
+    <a href="https://www.facebook.com/jahim.sankoh.5" target="_blank"><img src="facebook.png" alt="Facebook" width="32" height="32"></a>
+    <a href="https://www.instagram.com/jusbeing.jahim/" target="_blank"><img src="instagram.png" alt="Instagram" width="32" height="32"></a>
+    <a href="https://www.twitter.com/jahim4x" target="_blank"><img src="x.png" alt="X formely Twitter" width="32" height="32"></a>
                 </div>
                 </div>
                 </div>
@@ -54,45 +48,5 @@
         <footer>
             <p>&copy; 2023 Jahim Sankoh. All rights reserved.</p>
         </footer>
-</body>
-</html>kit, Chrome= Edge">
-<link rel="icon" type="image/x-icon" href="J.ico">
-<link rel="stylesheet" type="text/css" href="styles.css">
-<script type="text/javascript" src="fontsize.js"></script>
-</head>
-<body>
-
-  <header>
-<?php echo"<h1>Contact Information</h1>"; ?>
-<div class="wrapper">
-    <img src="IMG_20220531_122425_177.jpg" alt="IMG" width="405px" height="405px">
-    <div class="text-box">
-    <?php echo"<h2>Email Me:</h2>"; ?>
-    <?php echo"<p><li href="mailto:jahimsankoh319@gmail.com" target="_blank">jahimsankoh319@gmail.com</p></li>"; ?>
-    <?php echo"<h3>Phone:</h3>"; ?>
-    <?php echo"<p><li href="tel:803-846-0324" target="_blank">Call Me for Questions, Contributing to any of my Projects on GitHub, You can Reach Me Here: (803) 846-0324</p></li>"; ?>
-    <?php echo"<h4>Socials:</h4>"; ?>
-      <ul>
-      <li><a href="https://github.com/Loading617/" target="_blank"><img src="GitHub.png" alt="GitHub" width="32" height="32"></a></li>
-      <li><a href="https://www.facebook.com/jahim.sankoh.5" target="_blank"><img src="facebook.png" alt="Facebook" width="32" height="32"></a></li>
-      <li><a href="https://www.instagram.com/jusbeing.jahim/" target="_blank"><img src="instagram.png" alt="Instagram" width="32" height="32"></a></li>
-      <li><a href="https://www.twitter.com/jahim4x" target="_blank"><img src="x.png" alt="X formely Twitter" width="32" height="32"></a></li>
-      </ul>
-    <div class="navbar">
-        <div class="navbar-logo">Jahim Sankoh</div>
-        <nav>
-
-          <li href="home.php">Home</li>
-          <li href="funthings.php">Fun Things</li>
-          <li href="hobbies.php">Hobbies</li>
-          <li href="techstory.php">Tech Story</li>
-          <li href="contactme.php">Contact Me</li>
-        
-        </nav>
-    
-
-    </div>
-        
-</div>
 </body>
 </html>
